4339dbc642d08e156a7cce951a5608af2b7f8dc0
kierepkae <kierepkaedyta@gmail.com>
2019-11-21 17:23:16 +0100

    Initial check-in of module ArraysLists

---
+++ ArraysLists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArraysLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
