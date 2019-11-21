458d774e20a7c4238b226fb75ec7757d6afc9663
kierepkae <kierepkaedyta@gmail.com>
2019-11-21 18:00:35 +0100

    use method Length, IndexOf and Clear

--- ArraysLists/Program.cs
+++ ArraysLists/Program.cs
@@ -6,7 +6,28 @@ namespace ArraysLists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = { 3,7,9,2,14,6};
+
+            // Length
+
+            Console.WriteLine(numbers.Length);
+
+            // IndexOf()
+
+            int index = Array.IndexOf(numbers,9);
+            Console.WriteLine(index);
+
+            // Clear()
+
+            Array.Clear(numbers,0,2);
+
+            Console.WriteLine("Clear effect:");
+            foreach (var number in numbers){
+                Console.WriteLine(number);
+            }
         }
+
+
+
     }
 }
