ec5508a3b5e318f8c728d5028d2a4255cb74a1f7
kierepkae <kierepkaedyta@gmail.com>
2019-11-21 20:50:05 +0100

    use Remove, Count, IndexOf, LastIndex for list numbers2

--- ArraysLists/Program.cs
+++ ArraysLists/Program.cs
@@ -7,7 +7,7 @@ namespace ArraysLists
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 3,7,9,2,14,6};
+            int[] numbers = { 3, 7, 9, 2, 14, 6 };
 
             // Length
 
@@ -15,15 +15,15 @@ namespace ArraysLists
 
             // IndexOf()
 
-            int index = Array.IndexOf(numbers,9);
+            int index = Array.IndexOf(numbers, 9);
             Console.WriteLine(index);
 
             // Clear()
 
-            Array.Clear(numbers,0,2);
+            Array.Clear(numbers, 0, 2);
 
             Console.WriteLine("Clear effect:");
-            foreach (var number in numbers){
+            foreach (var number in numbers) {
                 Console.WriteLine(number);
             }
 
@@ -52,15 +52,29 @@ namespace ArraysLists
 
             // LISTS
 
-            var numbers2 = new List<int>() { 1,2,3,4};
+            var numbers2 = new List<int>() { 1, 2, 3, 4 };
             numbers2.Add(1);
-            numbers2.AddRange(new int[3] { 5,6,7});
+            numbers2.AddRange(new int[3] { 5, 6, 7 });
+            foreach (var num in numbers2) {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("Index of 1:" + numbers2.IndexOf(1));
+            Console.WriteLine("Last index of 1:" + numbers2.LastIndexOf(1));
+            Console.WriteLine("Count:" + numbers2.Count);
+            numbers2.Remove(1);
+            foreach (var number in numbers2)
+            {
+                Console.WriteLine(number);
+            }
+
+            for (var i = 0; i < numbers2.Count; i++) {
+                if (numbers2[i] == 1) {
+                    numbers2.Remove(numbers[i]);
+                }
+            }
 
         }
-
-
-
-
 
     }
 }
